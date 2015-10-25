39d21115f3e67ff8f7bcda8b7e02abbf0965fdef
Asterrea <theresamendoza1115@yahoo.com>
2015-10-25 16:56:48 -0700

    Update: Added Rotate Counter Clockwise

--- src/controller/EquationController.java
+++ src/controller/EquationController.java
@@ -14,6 +14,7 @@ public class EquationController {
 	private List<double[][]> dataPoints;
 	private String orientation;
 	private List<double[][]> transformedPoints;
+	private Matrix result;
 	
 	public EquationController(){
 		dataPoints = new ArrayList<double[][]>();
@@ -27,29 +28,29 @@ public class EquationController {
 		System.out.println(vDistance);
 		System.out.println(orientation);
 
-		for(int i = 0 ; i < dataPoints.size() ; i++){
-			Matrix matrix = new Matrix(dataPoints.get(i));
-			matrix.printMatrix();
-			System.out.println();
-		}
+//		for(int i = 0 ; i < dataPoints.size() ; i++){
+//			Matrix matrix = new Matrix(dataPoints.get(i));
+//			matrix.printMatrix();
+//			System.out.println();
+//		}
 		
-		translate(4, -3);
+		rotate_cc(90);
 	}
 	
+	//translation
 	public void translate(double x, double y){
 
 		double[][] trans_point = {{1,0,x},{0,1,y},{0,0,1}};
 		Matrix trans = new Matrix(trans_point);
-		Matrix result;
 		
 		for(int i = 0 ; i < dataPoints.size() ; i++){
 			Matrix matrix = new Matrix(dataPoints.get(i));
 			
-			System.out.println("Original Matrix: "); 
+			System.out.println("Original Point Matrix: "); 
 				matrix.printMatrix();
 				System.out.println();
 				
-			System.out.println("Translated Matrix: ");
+			System.out.println("Translated Point Matrix: ");
 			result = trans.multiply(matrix); 
 			
 			result.printMatrix();
@@ -57,6 +58,27 @@ public class EquationController {
 		}
 	}
 	
+	//counter clockwise rotation
+	public void rotate_cc(double degree){
+		
+		double radians = Math.toRadians(degree);
+		double[][] rotate_points = {{Math.cos(radians),-Math.sin(radians),0}, {Math.sin(radians), Math.cos(radians), 0}, {0,0,1}};
+		Matrix rotateMatrix = new Matrix(rotate_points);
+		for(int i = 0; i < dataPoints.size() ; i++){
+			Matrix matrix = new Matrix(dataPoints.get(i));
+			
+			System.out.println("Original Point Matrix: ");
+				matrix.printMatrix();
+				System.out.println();
+				
+			System.out.println("Translated Point Matrix: ");
+			result = rotateMatrix.multiply(matrix);
+			
+			result.printMatrix();
+			System.out.println();
+		}
+
+	}
 	
 	public String getType() {
 		return type;
